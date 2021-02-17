85be57232280c034dc485075c8ccb094b3f31849
Mario <razvan.mario95@gmail.com>
2021-02-17 21:42:21 +0200

    test1

---
+++ modul1_1/modul1_1.py
@@ -0,0 +1,26 @@
+print('print \u200b("")')
+
+# print ​("")
+
+print(print)
+print('3')
+
+print('My name is:' ,'Mario', '1', sep='_', end='*')
+print('My name is:' ,'Mario', '2', sep='_', end='#')
+print('My name is:' ,'Mario', '3', sep='_', end='@')
+
+print_text1 = 'Hello World 1'
+print_text2 = 'Hello World 2'
+print()
+print (print_text1, print_text2)
+
+print_text1 = 123
+print_text2 = 456
+
+print(print_text1, print_text2)
+
+print(' ','_','_',' ')
+print("|"," ",' ','|')
+print("|"," "," ","|")
+print("|"," "," ","|")
+print("|","_","_",'|')
