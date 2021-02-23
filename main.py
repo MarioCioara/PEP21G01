c3690ddfe1bf03ed79e1628ee52b27d646bfe042
Mario <razvan.mario95@gmail.com>
2021-02-24 01:33:59 +0200

    Main

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-print('Hello!')
-print()
-print('In which city did you grow up?')
-city = str(input())
-print()
-print('Name a pet please')
-pet = str(input())
-print()
-print(city + ' ' + pet)
 
 
